158b582950b40fe5b97c8cff5c09493d07da6961
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-06 12:30:10 +0300

    Defining a function

--- run.py
+++ run.py
@@ -3,5 +3,12 @@ from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
+@app.ruote('/')
+def index():
+    newsapi =NewsApiClient(api_key='6e766d66ef3d492ca4ffe25ebfbfa61e')
+    topheadlines=newsapi.get_top_headlines(sources='bbc-news')
+    articles
+
+
 if __name__=='__main__':
     app.run(debug =True)
